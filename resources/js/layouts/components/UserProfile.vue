3a7dfe42526d2bfbe14302f4832701c5140ed20c
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-05 00:28:05 +0300

    Send logout request when logging out in frontend

--- resources/js/layouts/components/UserProfile.vue
+++ resources/js/layouts/components/UserProfile.vue
@@ -1,24 +1,28 @@
 <script setup>
 import { initialAbility } from '@/plugins/casl/ability'
 import { useAppAbility } from '@/plugins/casl/useAppAbility'
+import axios from '@axios'
 
 const router = useRouter()
 const ability = useAppAbility()
 const userData = JSON.parse(localStorage.getItem('userData') || 'null')
 
 const logout = () => {
-  // Remove "userData" from localStorage
-  localStorage.removeItem('userData')
+  // Send logout request
+  axios.post('/auth/logout').then(r => {
+    // Remove "userData" from localStorage
+    localStorage.removeItem('userData')
 
-  // Remove "accessToken" from localStorage
-  localStorage.removeItem('accessToken')
-  router.push('/login').then(() => {
+    // Remove "accessToken" from localStorage
+    localStorage.removeItem('accessToken')
+    router.push('/login').then(() => {
 
-    // Remove "userAbilities" from localStorage
-    localStorage.removeItem('userAbilities')
+      // Remove "userAbilities" from localStorage
+      localStorage.removeItem('userAbilities')
 
-    // Reset ability to initial ability
-    ability.update(initialAbility)
+      // Reset ability to initial ability
+      ability.update(initialAbility)
+    })
   })
 }
 </script>
