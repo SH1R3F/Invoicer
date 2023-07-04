9591ee427f3caff13325f62423300f0fb41786c4
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-04 23:58:52 +0300

    Write tests for `/api/v1/user` endpoint

---
+++ tests/Feature/Api/Auth/UserTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Controllers\Api\V1\Auth\AuthController;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * It gets logged in user info
+     */
+    public function test_logged_in_user_info_endpoint(): void
+    {
+        // Create user
+        $user = User::factory()->create();
+
+        // Perform login
+        $response = $this->actingAs($user)->json('GET', action([AuthController::class, 'user']));
+
+        // Assert
+        $response
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'id' => $user->id,
+                'email' => $user->email,
+                'fullName' => $user->name,
+            ]);
+    }
+
+    /**
+     * It requires user to be logged in
+     */
+    public function test_logged_in_user_info_endpoint_middleware(): void
+    {
+        // Perform login
+        $response = $this->json('GET', action([AuthController::class, 'user']));
+
+        // Assert
+        $response
+            ->assertStatus(401)
+            ->assertExactJson([
+                'message' => 'Unauthenticated.'
+            ]);
+    }
+}
