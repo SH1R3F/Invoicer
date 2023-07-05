13d845abfb2bbc23e1ecf8fe5a37ba1a2f02a54b
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-05 20:24:11 +0300

    Translate validators

--- resources/js/@core/utils/validators.js
+++ resources/js/@core/utils/validators.js
@@ -1,11 +1,12 @@
+import i18n from '@/plugins/i18n'
 import { isEmpty, isEmptyArray, isNullOrUndefined } from './index'
 
 // 👉 Required Validator
 export const requiredValidator = value => {
   if (isNullOrUndefined(value) || isEmptyArray(value) || value === false)
-    return 'This field is required'
-  
-  return !!String(value).trim().length || 'This field is required'
+    return i18n.global.t('This field is required')
+
+  return !!String(value).trim().length || i18n.global.t('This field is required')
 }
 
 // 👉 Email Validator
@@ -14,30 +15,30 @@ export const emailValidator = value => {
     return true
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   if (Array.isArray(value))
-    return value.every(val => re.test(String(val))) || 'The Email field must be a valid email'
-  
-  return re.test(String(value)) || 'The Email field must be a valid email'
+    return value.every(val => re.test(String(val))) || i18n.global.t('The Email field must be a valid email')
+
+  return re.test(String(value)) || i18n.global.t('The Email field must be a valid email')
 }
 
 // 👉 Password Validator
 export const passwordValidator = password => {
   const regExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*()]).{8,}/
   const validPassword = regExp.test(password)
-  
+
   return (
     // eslint-disable-next-line operator-linebreak
     validPassword ||
-        'Field must contain at least one uppercase, lowercase, special character and digit with min 8 chars')
+    i18n.global.t('Field must contain at least one uppercase, lowercase, special character and digit with min 8 chars'))
 }
 
 // 👉 Confirm Password Validator
-export const confirmedValidator = (value, target) => value === target || 'The Confirm Password field confirmation does not match'
+export const confirmedValidator = (value, target) => value === target || i18n.global.t('The Confirm Password field confirmation does not match')
 
 // 👉 Between Validator
 export const betweenValidator = (value, min, max) => {
   const valueAsNumber = Number(value)
-  
-  return (Number(min) <= valueAsNumber && Number(max) >= valueAsNumber) || `Enter number between ${min} and ${max}`
+
+  return (Number(min) <= valueAsNumber && Number(max) >= valueAsNumber) || i18n.global.t(`Enter number between {min} and {max}`, { min, max })
 }
 
 // 👉 Integer Validator
@@ -45,9 +46,9 @@ export const integerValidator = value => {
   if (isEmpty(value))
     return true
   if (Array.isArray(value))
-    return value.every(val => /^-?[0-9]+$/.test(String(val))) || 'This field must be an integer'
-  
-  return /^-?[0-9]+$/.test(String(value)) || 'This field must be an integer'
+    return value.every(val => /^-?[0-9]+$/.test(String(val))) || i18n.global.t('This field must be an integer')
+
+  return /^-?[0-9]+$/.test(String(value)) || i18n.global.t('This field must be an integer')
 }
 
 // 👉 Regex Validator
@@ -59,16 +60,16 @@ export const regexValidator = (value, regex) => {
     regeX = new RegExp(regeX)
   if (Array.isArray(value))
     return value.every(val => regexValidator(val, regeX))
-  
-  return regeX.test(String(value)) || 'The Regex field format is invalid'
+
+  return regeX.test(String(value)) || i18n.global.t('The Regex field format is invalid')
 }
 
 // 👉 Alpha Validator
 export const alphaValidator = value => {
   if (isEmpty(value))
     return true
-  
-  return /^[A-Z]*$/i.test(String(value)) || 'The Alpha field may only contain alphabetic characters'
+
+  return /^[A-Z]*$/i.test(String(value)) || i18n.global.t('The Alpha field may only contain alphabetic characters')
 }
 
 // 👉 URL Validator
@@ -76,16 +77,16 @@ export const urlValidator = value => {
   if (isEmpty(value))
     return true
   const re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/
-  
-  return re.test(String(value)) || 'URL is invalid'
+
+  return re.test(String(value)) || i18n.global.t('URL is invalid')
 }
 
 // 👉 Length Validator
 export const lengthValidator = (value, length) => {
   if (isEmpty(value))
     return true
-  
-  return String(value).length === length || `The Min Character field must be at least ${length} characters`
+
+  return String(value).length === length || i18n.global.t(`The Min Character field must be at least {length} characters`, { length })
 }
 
 // 👉 Alpha-dash Validator
@@ -93,6 +94,6 @@ export const alphaDashValidator = value => {
   if (isEmpty(value))
     return true
   const valueAsString = String(value)
-  
-  return /^[0-9A-Z_-]*$/i.test(valueAsString) || 'All Character are not valid'
+
+  return /^[0-9A-Z_-]*$/i.test(valueAsString) || i18n.global.t('All Character are not valid')
 }
