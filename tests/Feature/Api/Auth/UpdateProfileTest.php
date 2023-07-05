b4cdd970c7b51c2a2dfe5d1b105f12682b741cbb
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-05 20:04:54 +0300

    Write tests for updating user password

--- tests/Feature/Api/Auth/UpdateProfileTest.php
+++ tests/Feature/Api/Auth/UpdateProfileTest.php
@@ -27,6 +27,53 @@ class UpdateProfileTest extends TestCase
 
         $response
             ->assertStatus(200)
+            ->assertJsonPath('message', 'Profile updated successfully')
             ->assertJsonPath('userData.email', $email);
+    }
+
+    /**
+     * Users can update their password
+     */
+    public function test_user_updates_their_password_successfully(): void
+    {
+        Sanctum::actingAs($user = User::factory()->create());
+
+        $response = $this->json('PUT', action([ProfileController::class, 'password']), [
+            'current_password'          => 'password',
+            'new_password'              => 'Password123!',
+            'new_password_confirmation' => 'Password123!',
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('message', 'Password updated successfully');
+    }
+
+    /**
+     * User's password validation works
+     *
+     * @dataProvider validatePasswords
+     */
+    public function test_user_password_updates_are_validated(array $data, string|array $expected): void
+    {
+        Sanctum::actingAs($user = User::factory()->create());
+
+        $response = $this->json('PUT', action([ProfileController::class, 'password']), $data);
+
+        $response
+            ->assertStatus(422)
+            ->assertSee($expected);
+    }
+
+    public function validatePasswords(): array
+    {
+        return [
+            // Required fields
+            [[], ['The current password field is required.', 'The new password field is required.']],
+            // Incorrect current password
+            [['current_password' => 'wrong password'], 'The current password is incorrect.'],
+            // New password validation
+            [['current_password' => 'password', 'new_password' => '0'], ['The new password field confirmation does not match.', 'The new password field must be at least 8 characters.', 'The new password field format is invalid.']],
+        ];
     }
 }
