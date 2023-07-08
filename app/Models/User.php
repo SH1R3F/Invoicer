dfd2af6862e8eec563c54cc868ae277fe7804264
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-09 00:50:51 +0300

    Add traits to `User` model & Add `scopeFilter` method

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\Orderable;
+use App\Traits\Searchable;
+use Illuminate\Http\Request;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes, Searchable, Orderable;
 
     protected $guard_name = 'sanctum';
 
@@ -50,4 +55,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Filter users in admin panel
+     */
+    public function scopeFilter(Builder $query, Request $request): void
+    {
+        $query->when($request->role, fn ($query, $role) => $query->whereHas('roles', fn ($query) => $query->where('name', $role)));
+    }
 }
