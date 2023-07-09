3f7e457634815512096ff47e34eb5bf9d7a3026b
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-09 16:00:52 +0300

    Create `ProductFactory`

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sku' => Str::random(8),
+            'name' => fake()->sentence,
+            'description' => fake()->paragraph(3),
+            'category_id' => Category::factory()->create()->id,
+            'price' => rand(100_00, 1000_00)
+        ];
+    }
+}
