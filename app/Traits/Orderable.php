2bf973de39957205424460fa4823637d8cdb9125
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-09 22:49:22 +0300

    Refactor orderable to order products relationship

--- app/Traits/Orderable.php
+++ app/Traits/Orderable.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
 
 trait Orderable
@@ -24,6 +25,9 @@ trait Orderable
                     break;
                 case 'role':
                     $this->applyRoleOrder($order, $query);
+                    break;
+                case 'category':
+                    $this->applyCategoryOrder($order, $query);
                     break;
                 default:
                     if (in_array($key, \Illuminate\Support\Facades\Schema::getColumnListing($this->getTable()))) {
@@ -47,6 +51,17 @@ trait Orderable
                 return $query->from('model_has_roles')
                     ->whereRaw("`model_has_roles`.model_id = `users`.id")
                     ->select('role_id');
+            }, $order);
+        }
+    }
+
+    private function applyCategoryOrder(string $order, Builder $query)
+    {
+        if ($this instanceof Product) {
+            $query->orderBy(function ($query) {
+                return $query->from('categories')
+                    ->whereRaw("`categories`.id = `products`.category_id")
+                    ->select('name');
             }, $order);
         }
     }
