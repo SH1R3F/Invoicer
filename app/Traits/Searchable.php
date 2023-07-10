70f29006e2bcf0433dbf033b16d6c149c9031842
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-10 18:56:33 +0300

    Refactor `Searchable` trait (Support searching nested relationships)

--- app/Traits/Searchable.php
+++ app/Traits/Searchable.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use App\Models\Quote;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Builder;
 
 trait Searchable
@@ -14,9 +16,19 @@ trait Searchable
         if ($search) {
             $query->where(function ($query) use ($columns, $search) {
                 foreach ($columns as $column) {
-                    $query->orWhere($column, 'LIKE', "%{$search}%");
+                    if (Str::contains($column, '.')) {
+                        $this->searchRelationship($query, $column, $search);
+                    } else {
+                        $query->orWhere($column, 'LIKE', "%{$search}%");
+                    }
                 }
             });
         }
+    }
+
+    private function searchRelationship(Builder &$query, string $column, string $search): void
+    {
+        [$relation, $attribute] = explode('.', $column);
+        $query->whereHas($relation, fn ($query) => $query->where($attribute, 'LIKE', "%{$search}%"));
     }
 }
