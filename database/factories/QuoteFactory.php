d0ab62fc136dd516aee195be30988643f02901cf
Mahmoud Shiref <sh1r3f@gmail.com>
2023-07-10 18:01:26 +0300

    Create `QuoteFactory`

---
+++ database/factories/QuoteFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\QuoteStatus;
+use App\Models\Quote;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quote>
+ */
+class QuoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'        => User::factory()->create()->id,
+            'quote_number'   => str_pad(((string)((int)Quote::latest()->first()?->quote_number ?? 0) + 1), 4, '0', STR_PAD_LEFT),
+            'quote_date'     => fake()->date,
+            'due_date'       => fake()->date,
+            'status'         => fake()->randomElement(QuoteStatus::cases()),
+            'discount_type'  => $type = rand(0, 1) ? fake()->randomElement(['fixed', 'percentage']) : null,
+            'discount_value' => $type ? rand(1, 100) : 0,
+            'notes'          => rand(0, 1) ? fake()->paragraph(rand(1, 7)) : null
+        ];
+    }
+}
